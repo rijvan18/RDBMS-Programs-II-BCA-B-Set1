27a22c5c872962bd419e3e910d97328cf557d074
rijvan18 <mohammedrijvan93600@gmail.com>
2026-09-22 12:36:38 +0530

    Update solution.sql

--- Assignment10_Left_Right_Join/solution.sql
+++ Assignment10_Left_Right_Join/solution.sql
@@ -7,3 +7,31 @@
 -- LEFT JOIN
 
 -- RIGHT JOIN
+CREATE TABLE Course (
+    CourseID INT PRIMARY KEY,
+    CourseName VARCHAR(50),
+    Credits INT
+);
+
+INSERT INTO Course VALUES
+(201, 'Database Systems', 4),
+(202, 'Data Structures', 2),
+(203, 'Mathematics', 4);
+
+CREATE TABLE Enrollment (
+    EnrollmentID INT PRIMARY KEY,
+    StudentID INT,
+    CourseID INT
+);
+
+INSERT INTO Enrollment VALUES
+(1, 1001, 201),
+(2, 1001, 202),
+(3, 1002, 203),
+(4, 1003, 201);
+
+SELECT Course.CourseID, Course.CourseName,
+       Enrollment.EnrollmentID, Enrollment.StudentID
+FROM Course
+LEFT JOIN Enrollment
+ON Course.CourseID = Enrollment.CourseID;
