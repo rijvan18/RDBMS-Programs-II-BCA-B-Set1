8b16f5fb71131611ccdea8a1aac4bdf1b9b8aa79
rijvan18 <mohammedrijvan93600@gmail.com>
2026-09-22 12:31:59 +0530

    Update solution.sql

--- Assignment07_Marksheet/solution.sql
+++ Assignment07_Marksheet/solution.sql
@@ -5,3 +5,21 @@
 -- Display students with Marks > 80
 
 -- Sort by Marks DESC
+CREATE TABLE Marksheet (
+    RollNo INT,
+    Name VARCHAR(50),
+    Department VARCHAR(20),
+    Marks INT
+);
+
+INSERT INTO Marksheet VALUES
+(2, 'Arun', 'CSE', 85),
+(3, 'Karthik', 'CSE', 78),
+(4, 'Nisha', 'ECE', 92),
+(5, 'Rahul', 'IT', 67),
+(6, 'Latha', 'IT', 88);
+
+SELECT *
+FROM Marksheet
+WHERE Marks > 80
+ORDER BY Marks DESC;
